ba98a2d3272565d102a8352a008c589ec6051954
Alex.Mills <amills@vibeirl.com>
2024-01-16 16:42:51 -0500

    agage

--- go.mod
+++ go.mod
@@ -2,6 +2,13 @@ module github.com/oresoftware/cmd-line-parser
 
 go 1.21.5
 
-require github.com/oresoftware/json-logging v0.0.0-20231214045355-14e52e441216 // indirect
+require github.com/oresoftware/json-logging v0.0.0-20231214045355-14e52e441216
+
+require (
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381 // indirect
+	golang.org/x/crypto v0.0.0-20200204104054-c9f3fb736b72 // indirect
+	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
+)
 
 replace github.com/oresoftware/json-logging => ../json-logging
--- v1/clp/log.go
+++ v1/clp/log.go
@@ -1,11 +1,12 @@
 package clp
 
 import (
-	json_logging "github.com/oresoftware/json-logging/jlog"
+	json_logging "github.com/oresoftware/json-logging/jlog/lib"
+	"github.com/oresoftware/json-logging/jlog/shared"
 	"os"
 )
 
 var appName = os.Getenv("app_name")
 
-var Stdout = json_logging.New(appName, "", json_logging.TRACE, []*json_logging.FileLevel{})
-var Stderr = json_logging.New(appName, "", json_logging.WARN, []*json_logging.FileLevel{})
+var Stdout = json_logging.CreateLogger(appName).SetLogLevel(shared.TRACE)
+var Stderr = json_logging.CreateLogger(appName).SetLogLevel(shared.WARN)
